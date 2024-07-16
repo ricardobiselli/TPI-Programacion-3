46bd4cc5a9d954e618a9f85438f62d10174820f3
Luciano Brion <luciano.brion@hotmail.com>
2024-07-15 21:37:35 -0300

    Cambios ReposityBase

--- Application/IRepositories/IRepositoryBase.cs
+++ Application/IRepositories/IRepositoryBase.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Application.IRepositories
 {
-    internal interface IRepositoryBase
+    public interface IRepositoryBase<T> where T : class
     {
+        public Task <List<T>> GetAllAsync();
+
+        public Task<T?> GetByIdAsync<Tid>(Tid id);
+
+        public Task<T> AddAsync(T entity);
+
+        public Task<int> UpdateAsync(T entity);
+        public Task<int> DeleteAsync(T entity);
+
+
+
     }
 }
+   
